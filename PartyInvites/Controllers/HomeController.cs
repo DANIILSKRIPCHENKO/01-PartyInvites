1b773eafb08f05032c114d9bf25fd8391d859f84
”Daniil <das76@tpu.ru>
2021-11-05 07:16:51 +0700

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index () 
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View(); // так как нет аргументов то визуализируется стандартное представление 
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set;} // Может принимать true false or null 
+
+    }
+}
