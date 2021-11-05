04f49881c6493d959ca13765426ad63dff1c6181
”Daniil <das76@tpu.ru>
2021-11-05 07:46:23 +0700

    List of people attending the party was add

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using PartyInvites.Models;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
@@ -12,9 +14,22 @@ namespace PartyInvites.Controllers
             return View("MyView");
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View(); // так как нет аргументов то визуализируется стандартное представление 
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse questResponse)
+        {
+            Repository.AddResponse(questResponse);
+            return View("Thanks", questResponse); 
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
